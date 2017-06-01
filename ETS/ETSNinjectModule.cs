b478c2bdd9383868cea7d8563421ef88acd460c5
NISHANTDHARKAR\Nishant Dharkar <nishantdharkar@gmail.com>
2017-06-02 01:48:10 +0530

    Created new git repository and pushed code

---
+++ ETS/ETSNinjectModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject.Modules;
+
+namespace ETS
+{
+    public class ETSNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            
+        }
+    }
+}
